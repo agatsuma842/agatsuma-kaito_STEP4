7a3f3ac64e564b0a4eab9f2f438c1f556fd30ffb
agatsuma <gv705@icloud.com>
2026-08-26 22:22:23 +0900

    提出物以外のファイルを削除

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM php:8.3-apache
-
-COPY src/ /var/www/html/
-
-COPY src/practice.php /var/www/html/
--- src/sample49.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function greet($name){
-    echo "Hello, " . $name . "！";
-}
-
-greet("Cytech");
-
-?>
